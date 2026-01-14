af533c9dbf06482c32f20a3ba5e379eaa93801d6
Rupam Bhakta <rupambhakta2020@gmail.com>
2026-01-14 13:11:10 +0530

    Clean the data, implement item.py, spider, pipeline settings

--- Freecodecamp/Bookscraper/pipelines.py
+++ Freecodecamp/Bookscraper/pipelines.py
@@ -8,6 +8,66 @@
 from itemadapter import ItemAdapter
 
 
-class FreecodecampPipeline:
+class BookScraperPipeline:
     def process_item(self, item, spider):
+        
+        adapter = ItemAdapter(item)
+
+        def first_or_self(value):
+            if isinstance(value, (list, tuple)):
+                return value[0] if value else ""
+            return value
+        
+        ## Strip all whitespace from strings
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != 'description':
+                value = first_or_self(adapter.get(field_name))
+                adapter[field_name] = value.strip() if isinstance(value, str) else value
+        
+        ## Category $ Product Type --> switch to lowercase
+        lowercase_keys = ['category','product_type']
+        for lowercase_key in lowercase_keys:
+            value = first_or_self(adapter.get(lowercase_key))
+            adapter[lowercase_key] = value.lower() if isinstance(value, str) else value
+        
+        ## Price --> convert to flore
+        price_keys = ['price','price_excl_tax','price_incl_tax','tax']
+        for price_key in price_keys:
+            value = first_or_self(adapter.get(price_key))
+            if isinstance(value, str):
+                value = value.replace('£','').replace('?','').strip()
+            adapter[price_key] = float(value) if value != "" else 0.0
+        
+        ## Availability --> extract number of books in stock
+        availability_string = first_or_self(adapter.get('availability'))
+        split_string_array = availability_string.split('(')
+        if len(split_string_array)<2:
+            adapter['availability'] = 0
+        else:
+            availability_array = split_string_array[1].split(' ')
+            adapter['availability'] = int(availability_array[0])
+        
+        ## Reviews --> convert string to number
+        num_reviews_string = first_or_self(adapter.get('num_reviews'))
+        adapter['num_reviews'] = int(num_reviews_string) if num_reviews_string != "" else 0
+        
+        ## Stars --> convert text to number
+        stars_string = first_or_self(adapter.get('stars'))
+        split_stars_array = stars_string.split(' ')
+        stars_text_value = split_stars_array[1].lower()
+        if stars_text_value == "zero":
+            adapter['stars'] = 0
+        elif stars_text_value == "one":
+            adapter['stars'] = 1
+        elif stars_text_value == "two":
+            adapter['stars'] = 2
+        elif stars_text_value == "three":
+            adapter['stars'] = 3
+        elif stars_text_value == "four":
+            adapter['stars'] = 4
+        elif stars_text_value == "five":
+            adapter['stars'] = 5
+
+        
         return item
--- Freecodecamp/Bookscraper/settings.py
+++ Freecodecamp/Bookscraper/settings.py
@@ -58,9 +58,9 @@ DOWNLOAD_DELAY = 1
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "Bookscraper.pipelines.BookscraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "Bookscraper.pipelines.BookScraperPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
